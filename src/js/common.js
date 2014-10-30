47a6b32f9c137b29437293712995639c91dcb4d9
Kutnayk Evgeniy <ifgen09@gmail.com>
2014-10-31 00:20:23 +0200

    фильтр

--- src/js/common.js
+++ src/js/common.js
@@ -3,5 +3,36 @@
  */
 $(document).ready(function(){
 	$('select').selectric();
-	$('#jqslider').jqslider({ theme:'steel' });
+	$("#example_id").ionRangeSlider({
+		min: 1000000,
+		max: 100000000,
+		type: "double",
+		postfix: " pounds",
+		step: 10000,
+		from: 25000000,
+		to: 35000000,
+		onChange: function(obj) {
+
+		},
+		onLoad: function(obj) {
+
+		},
+		onFinish: function() {
+			var filterValue = $("#example_id").val();
+			$('<div class="goods_preload"><img src="imgs/goods_preloader.gif" alt=""/></div>').prependTo($('.goods_wraper')).fadeIn()
+			$.ajax({
+				url: '',
+				type: 'POST',
+				data: filterValue,
+				dataType: 'text',
+				success: function(response){
+					//$('.goods_wraper').html('correct response');
+				},
+				error: function(xhr,ajaxOptions,thrownError) {
+					alert(thrownError)
+				}
+			})
+
+		}
+	});
 });
